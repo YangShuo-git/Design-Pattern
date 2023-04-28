501d5f3499cda37f72422b55bc63bd2a9a80fd7a
yangshuo <17801003317@163.com>
2023-04-28 23:35:27 +0800

    Proxy模式

--- src/ProducerAndConsumer/ProducerAndConsumer.cpp
+++ src/ProducerAndConsumer/ProducerAndConsumer.cpp
@@ -1,3 +1,5 @@
+// 生产者  ----->   队列（listBuffer）   ----->  消费者
+
 #include <thread>
 #include <mutex>
 #include <condition_variable>
@@ -16,8 +18,8 @@ private:
     condition_variable bufferCanRead;     // 条件变量：可以读数据了
     condition_variable bufferCanWrite;    // 条件变量：可以生产数据了
     
-    const int dataSize = 10;    // 产生的总数据量
-    queue<int> listBuffer;      // 数据缓存区
+    const int dataSize = 10;    // 要产生的总数据量
+    queue<int> listBuffer;      // 数据缓存区（队列）
     const int bufferSize = 5;   // 缓存区大小
 };
 
@@ -27,12 +29,12 @@ void ProducerAndConsumerDemo::produceNumber()
     {
         {
             unique_lock<mutex> locker(m_mutex);
-            bufferCanWrite.wait(locker, [&]() { return listBuffer.size() < bufferSize; }); // 缓存区满了则阻塞
+            bufferCanWrite.wait(locker, [&]() { return listBuffer.size() < bufferSize; }); // 队列满了则阻塞（不要生产了）
             listBuffer.push(i);
             cout << "Producer---produce number：" << i << ", bufferSize:" << listBuffer.size() << endl;
         }  // 离开作用域，解锁互斥量
 
-        bufferCanRead.notify_one();  // 通知消费者，可以消费数据
+        bufferCanRead.notify_one();  // 队列有数据了，通知消费者可以消费数据
         this_thread::sleep_for(chrono::milliseconds(10));  // 释放cpu占用
     }
 }
@@ -43,14 +45,14 @@ void ProducerAndConsumerDemo::consumeNumber()
     {
         {
             unique_lock<mutex> locker(m_mutex);
-            bufferCanRead.wait(locker, [&]() {return !listBuffer.empty(); }); // 缓冲区为空则阻塞
+            bufferCanRead.wait(locker, [&]() {return !listBuffer.empty(); }); // 队列为空则阻塞，不要再消费了
            
             int i = listBuffer.front();
             listBuffer.pop();
             cout << "Consumer---consume number：" << i << ", bufferSize:" << listBuffer.size() << endl;
         }  // 解锁互斥量
 
-        bufferCanWrite.notify_one(); // 通知生产者，可以生产数据
+        bufferCanWrite.notify_one(); // 队列没有数据了，通知生产者可以生产数据
     }
 }
 
