2b639aff9e877cecce14907abf7c3de681e4fc0e
yangshuo <17801003317@163.com>
2023-10-25 23:58:35 +0800

    优化适配器

--- src/Adapter/adapter.cpp
+++ src/Adapter/adapter.cpp
@@ -1,88 +1,71 @@
 #include <iostream>
+#include <string>
+ 
 using namespace std;
-
-// 对象适配器
-// 1. 以PS2转USB转化器（适配器）为例
-// PS2接口
-class PS2{
+ 
+ 
+// 尼康相机类（适配者）
+class NikonCamera
+{
 public:
-    virtual ~PS2(){}
-    virtual void isPS2() = 0;
-};
-// USB接口
-class USB{
-public:
-    virtual void idUSB() = 0 ;
-};
-// USB实现
-class USBer : public USB {
-public:
-    void idUSB() override {
-        cout<<"This is a USB"<<endl;
+    // 打开相机
+    void openNikonCamera() {
+        cout << "尼康相机：打开相机。" << endl;
     }
-};
-// 适配器
-class Adapter : public PS2{
-private:
-    USB* usb;
-
-public:
-    Adapter(USB* usb)
-    {
-        this->usb = usb;
+    // 关闭相机
+    void closeNikonCamera() {
+        cout << "尼康相机：关闭相机。" << endl;
     }
-
-    void isPS2() override {
-        usb->idUSB();
+    // 打开曝光设置
+    void openExposureSettings() {
+        cout << "尼康相机：打开曝光设置。" << endl;
     }
 };
 
-/*
- * 例2 对象适配器模式
- * 角色：Target Adaptee Adapter
- * 客户端需要调用Request方法，而Adaptee类中没有该方法，为了使客户端能够使用Adaptee类
- * 需要提供一个包装（Wrapper）类Adapter，这个包装类包装了一个Adaptee的实例，从而将客户端
- * 与Adaptee衔接了起来。由于Adapter与Adaptee是委派关系
- */
-// Target接口
-class Target{
+// 相机接口类
+class Camera
+{
 public:
-    virtual void Request()=0;
+    // 构造函数
+    Camera() {};
+    // 析构函数
+    virtual ~Camera() {};
+    // 打开相机
+    virtual void openCamera() = 0;
+    // 关闭相机
+    virtual void closeCamera() = 0;
+    // 配置属性
+    virtual void setConfig() = 0;
 };
-
-// Adaptee类
-class Adaptee{
+ 
+// 相机适配器类
+class CameraAdapter :public Camera, public NikonCamera
+{
 public:
-    void SpecificRequest()
-    {
-        cout<<"Called SpecificRequest()"<<endl;
+    // 打开相机
+    virtual void openCamera() {
+        openNikonCamera();
+    }
+    // 关闭相机
+    virtual void closeCamera() {
+        closeNikonCamera();
+    }
+    // 配置属性
+    virtual void setConfig() {
+        // 打开曝光设置
+        openExposureSettings();
     }
 };
-
-// Adapter类
-class Adapter2 : public Target {
-private:
-    Adaptee* adaptee_;
-public:
-    Adapter2(Adaptee* a)
-    {
-        this->adaptee_ = a;
-    }
-
-    void Request() override {
-        adaptee_->SpecificRequest();
-    }
-};
-
 
 int main()
 {
-    USB* usb = new USBer();
-    PS2* p = new Adapter(usb);
-    p->isPS2();
-
-    Adaptee* aa = new Adaptee();
-    Target *t = new Adapter2(aa);
-    t->Request();
-    return 0;
+     Camera *m_camera = new CameraAdapter();
+     // 打开相机
+     m_camera->openCamera();
+     // 配置相机
+     m_camera->setConfig();
+     // 关闭相机
+     m_camera->closeCamera();
+     delete m_camera;
+     return 0;
 }
